b65d97dd6b9c27f564eaa198ababa0db0abbb072
Marlen <titanyforgame@gmail.com>
2024-08-14 14:11:49 +0300

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"<h1>Hello Happy Python</h1>")
+
+
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()
